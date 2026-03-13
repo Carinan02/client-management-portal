07b2c2f8e4a9cca34dd67c37931b1be76dfa8ffd
Carinan02 <carinan02@gmail.com>
2026-03-13 17:40:06 +0800

    added a new logs route edited

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@ use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ImportController;
 use Illuminate\Support\Facades\DB;
 
+
+Route::get('/db-test', function () {
+    try {
+        DB::connection()->getPdo();
+        return 'DB Connected! ✅ Database: ' . DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        return 'DB NOT Connected ❌ Error: ' . $e->getMessage();
+    }
+});
+
+Route::get('/show-log', function () {
+    $log = storage_path('logs/laravel.log');
+    if (file_exists($log)) {
+        return response()->file($log);
+    }
+    return 'No log file found';
+});
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -59,20 +77,5 @@ Route::middleware('role:admin,manager')->group(function () {
         Route::get('imports/latest', [ImportController::class, 'latest'])->name('imports.latest');
     });
 
-Route::get('/db-test', function () {
-    try {
-        DB::connection()->getPdo();
-        return 'DB Connected! ✅ Database: ' . DB::connection()->getDatabaseName();
-    } catch (\Exception $e) {
-        return 'DB NOT Connected ❌ Error: ' . $e->getMessage();
-    }
-});
 
-Route::get('/show-log', function () {
-    $log = storage_path('logs/laravel.log');
-    if (file_exists($log)) {
-        return response()->file($log);
-    }
-    return 'No log file found';
-});
 require __DIR__.'/auth.php';
